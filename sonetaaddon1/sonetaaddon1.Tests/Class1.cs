6173e52f2664fcbbff767ead9ccc243cd0ff256e
lukasz <lukasz@DESKTOP-2MH4PN1>
2022-11-19 22:55:23 +0100

    Add project files.

---
+++ sonetaaddon1/sonetaaddon1.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sonetaaddon1.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void test1()
+        {
+            Assert.IsNull(null);
+        }
+    }
+}
